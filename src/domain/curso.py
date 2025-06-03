14b2eeb1deab558e410704beed82488e252bddd1
leoferamos <fernandesramosleonardo@gmail.com>
2025-06-03 13:50:37 -0300

    feat(curso): add categoria and grau parameters to __init__ and update __repr__ method

--- src/domain/curso.py
+++ src/domain/curso.py
@@ -1,8 +1,10 @@
 class Curso:
-    def __init__(self, nome, codigo, id=None):
+    def __init__(self, nome, codigo, categoria=None, grau=None, id=None):
         self.id = id
         self.nome = nome
         self.codigo = codigo
+        self.categoria = categoria
+        self.grau = grau
 
     def __repr__(self):
-        return f"Curso(nome={self.nome!r}, codigo={self.codigo!r})"
+        return f"Curso(nome={self.nome!r}, codigo={self.codigo!r}, categoria={self.categoria!r}, grau={self.grau!r})"
